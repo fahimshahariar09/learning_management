9cfacc75da548ae19df916e5b6725dac2cddb0ac
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-03 22:17:11 -0800

    first commit

--- lib/controller/ui_controller/splash/splash.dart
+++ lib/controller/ui_controller/splash/splash.dart
@@ -1,4 +1,16 @@
 
 import 'package:get/get.dart';
 
-class SplashScreenController extends GetxController{}
+class SplashScreenController extends GetxController{
+
+  Future nextPage() async{
+    await Future.delayed(Duration(seconds: 3));
+  }
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+
+}
