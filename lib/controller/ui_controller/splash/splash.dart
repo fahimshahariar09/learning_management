0efa7e5f06beaacc9336747188f83a0a01740aab
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-03 22:19:29 -0800

    first commit

--- lib/controller/ui_controller/splash/splash.dart
+++ lib/controller/ui_controller/splash/splash.dart
@@ -4,12 +4,11 @@ import 'package:get/get.dart';
 class SplashScreenController extends GetxController{
 
   Future nextPage() async{
-    await Future.delayed(Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 3));
   }
 
   @override
   void onInit() {
-    // TODO: implement onInit
     super.onInit();
   }
 
--- lib/view/screen/splash/splash_screen.dart
+++ lib/view/screen/splash/splash_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning_management/controller/ui_controller/splash/splash.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GetBuilder<SplashScreenController>(
+        init: SplashScreenController(),
+        builder: (context) {
+          return SafeArea(child: Text("data"));
+        });
   }
 }
