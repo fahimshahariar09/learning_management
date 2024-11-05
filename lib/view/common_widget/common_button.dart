86eec8782355d37b2020c2152f36d648a618b39a
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-04 23:41:59 -0800

    first commit

--- lib/view/common_widget/common_button.dart
+++ lib/view/common_widget/common_button.dart
@@ -3,10 +3,18 @@ import 'package:learning_management/view/common_widget/common_text.dart';
 
 class CommonButton extends StatelessWidget {
   const CommonButton(
-      {super.key, required this.onTap, required this.buttonName});
+      {super.key,
+      required this.onTap,
+      required this.buttonName,
+      this.buttonHeight,
+      this.buttonWidth,
+      this.buttonColor});
 
   final VoidCallback onTap;
   final String buttonName;
+  final double? buttonHeight;
+  final double? buttonWidth;
+  final Color? buttonColor;
 
   @override
   Widget build(BuildContext context) {
