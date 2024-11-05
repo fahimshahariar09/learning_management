59199572bd2faa91cc2f6d46eb4ec1c0b5e05f0c
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-04 23:45:22 -0800

    first commit

---
+++ lib/view/common_widget/common_loading_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommonLoadingButton extends StatelessWidget {
+  const CommonLoadingButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
