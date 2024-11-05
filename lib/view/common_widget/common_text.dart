fe5455e28f2e90e5a546035ec4b03f787c9b2794
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-04 23:36:29 -0800

    first commit

--- lib/view/common_widget/common_text.dart
+++ lib/view/common_widget/common_text.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+
 class CommonText extends StatelessWidget {
-  const CommonText({super.key});
+  const CommonText(
+      {super.key,
+      required this.titel,
+      this.color,
+      this.fWeight,
+      this.overflow});
+
+  final String titel;
+  final Color? color;
+  final FontWeight? fWeight;
+  final TextOverflow? overflow;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       '',
       style: TextStyle(
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
-        fontSize: 15,
-        overflow: TextOverflow.ellipsis
-      ),
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+          overflow: TextOverflow.ellipsis),
       maxLines: 1,
     );
   }
