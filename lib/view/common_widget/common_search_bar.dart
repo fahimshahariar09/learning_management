48d10c97bf84dcdf5948326798a072330edd1a7c
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-05 23:15:56 -0800

    first commit

--- lib/view/common_widget/common_search_bar.dart
+++ lib/view/common_widget/common_search_bar.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CommonSearchBar extends StatelessWidget {
-  const CommonSearchBar({super.key});
+  const CommonSearchBar({super.key, required this.hText});
+
+  final String hText;
 
   @override
   Widget build(BuildContext context) {
-    return TextField();
+    return TextField(
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        hintText: hText,
+      ),
+    );
   }
 }
