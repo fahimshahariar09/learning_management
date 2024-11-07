1c4026066437d5f2bc1b269ca20d32610b26f6b5
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-06 23:48:25 -0800

    first commit

--- lib/view/auth/widget/password_text_field.dart
+++ lib/view/auth/widget/password_text_field.dart
@@ -7,22 +7,30 @@ class PasswordTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextFormField(
       decoration: InputDecoration(
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            color: Colors.red,
+            width: 1,
           ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: const BorderSide(
-              color: Colors.red,
-              width: 1,
-            ),
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-          )),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        hintText: "password",
+      ),
+      validator: (value) {
+        if (value == '' || value == null) {
+          return "password can't be empty";
+        }
+        return null;
+      },
     );
   }
 }
