59dc1604fbdf46ced38464e3af8ab58834482807
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-06 23:36:00 -0800

    first commit

--- lib/view/auth/widget/email_text_field.dart
+++ lib/view/auth/widget/email_text_field.dart
@@ -5,6 +5,26 @@ class EmailTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return TextFormField(
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            color: Colors.red,
+            width: 1,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        hintText: "Name",
+      ),
+    );
   }
 }
