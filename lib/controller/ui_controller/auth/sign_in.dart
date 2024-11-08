5cf8cdd9eb994f669dbff1bb2e234eb8ade05545
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-07 23:01:54 -0800

    first commit

--- lib/controller/ui_controller/auth/sign_in.dart
+++ lib/controller/ui_controller/auth/sign_in.dart
@@ -1,5 +1,11 @@
 
 
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
-class   SignInController extends GetxController{}
+class   SignInController extends GetxController{
+  final formKey = GlobalKey<FormState>();
+  TextEditingController emailController =TextEditingController();
+  TextEditingController passController =TextEditingController();
+  RxBool isLoading = true.obs;
+}
