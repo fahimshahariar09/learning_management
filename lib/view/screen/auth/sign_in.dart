573832d9399cb35d67d319bd4b5dc823c7e905e1
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-07 23:14:19 -0800

    first commit

--- lib/view/screen/auth/sign_in.dart
+++ lib/view/screen/auth/sign_in.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning_management/controller/ui_controller/auth/sign_in.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SignInController controller =Get.put(SignInController());
     return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
     );
   }
 }
