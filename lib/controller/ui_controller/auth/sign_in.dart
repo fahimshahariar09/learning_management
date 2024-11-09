c703548bb275643eb1b4aca903f33c138bd051df
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-08 22:10:28 -0800

    first commit

--- lib/controller/ui_controller/auth/sign_in.dart
+++ lib/controller/ui_controller/auth/sign_in.dart
@@ -1,16 +1,25 @@
-
-
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:learning_management/controller/api_controller/auth/sign_in.dart';
 
-class   SignInController extends GetxController{
+class SignInController extends GetxController {
   final formKey = GlobalKey<FormState>();
-  TextEditingController emailController =TextEditingController();
-  TextEditingController passController =TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
   RxBool isLoading = true.obs;
 
-  signInFun()async{
-    isLoading.value=true;
-    isLoading.value=false;
+  signInFun() async {
+    isLoading.value = true;
+    SignInService.signinService(
+        email: emailController.text, password: passController.text);
+    isLoading.value = false;
   }
+
+  @override
+  void onInit() {
+    emailController.text="fahim@gamil.com";
+    passController.text="123456";
+    super.onInit();
+  }
+
 }
