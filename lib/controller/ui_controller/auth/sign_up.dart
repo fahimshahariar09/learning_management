d0188cc59c76f39008b5902cff89bd7405c18698
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-08 22:32:27 -0800

    first commit

--- lib/controller/ui_controller/auth/sign_up.dart
+++ lib/controller/ui_controller/auth/sign_up.dart
@@ -1,11 +1,18 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:learning_management/controller/api_controller/auth/sign_up.dart';
 
-class SignUpController extends GetxController{
-  TextEditingController nameController =TextEditingController();
-  TextEditingController emailController =TextEditingController();
-  TextEditingController passwordController =TextEditingController();
-  TextEditingController conpasswordController =TextEditingController();
+class SignUpController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController conpasswordController = TextEditingController();
+  RxBool isLoading = true.obs;
+
+  signupFun() async {
+    isLoading.value=true;
+    SignUpService.signUpService(name: nameController.text, email: emailController.text, password: passwordController.text, conpassword: conpasswordController.text);
+  }
 }
