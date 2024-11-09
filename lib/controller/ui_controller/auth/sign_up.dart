bfc6dae860ac8adbfd4cb19ab436e550ceca426a
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-08 22:29:29 -0800

    first commit

--- lib/controller/ui_controller/auth/sign_up.dart
+++ lib/controller/ui_controller/auth/sign_up.dart
@@ -1,5 +1,11 @@
 
 
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SignUpController extends GetxController{}
+class SignUpController extends GetxController{
+  TextEditingController nameController =TextEditingController();
+  TextEditingController emailController =TextEditingController();
+  TextEditingController passwordController =TextEditingController();
+  TextEditingController conpasswordController =TextEditingController();
+}
