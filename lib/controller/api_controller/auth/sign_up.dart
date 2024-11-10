888c7f22d515ff5325d87b0d3df65c048da7bf7e
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-09 23:29:24 -0800

    first commit

--- lib/controller/api_controller/auth/sign_up.dart
+++ lib/controller/api_controller/auth/sign_up.dart
@@ -1,14 +1,14 @@
 import 'dart:developer';
 
 class SignUpService {
-  static Future signUpService({
-    required String name,
-    required String email,
-    required String password,
-    required String conpassword,
-  }) async {
+  static Future signUpService(
+      {required String name,
+      required String email,
+      required String password,
+      required String conpassword}) async {
     try {
       await Future.delayed(const Duration(seconds: 3));
+      return true;
     } catch (e) {
       log("error :$e");
     }
--- lib/view/screen/auth/sign_up.dart
+++ lib/view/screen/auth/sign_up.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:learning_management/controller/ui_controller/auth/sign_up.dart';
+import 'package:learning_management/view/common_widget/common_button.dart';
 import 'package:learning_management/view/screen/auth/widget/con_password_field.dart';
 import 'package:learning_management/view/screen/auth/widget/email_text_field.dart';
 import 'package:learning_management/view/screen/auth/widget/name_text_field.dart';
 import 'package:learning_management/view/screen/auth/widget/password_text_field.dart';
+import 'package:learning_management/view/screen/home_screen/home_screen.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    SignUpController controller =Get.put(SignUpController());
+    SignUpController controller = Get.put(SignUpController());
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -24,9 +26,20 @@ class SignUp extends StatelessWidget {
                 const SizedBox(height: 15),
                 EmailTextField(emailController: controller.emailController),
                 const SizedBox(height: 15),
-                PasswordTextField(passwordController: controller.passwordController),
+                PasswordTextField(
+                    passwordController: controller.passwordController),
                 const SizedBox(height: 15),
-                ConPasswordField(conpasswordController: controller.conpasswordController),
+                ConPasswordField(
+                    conpasswordController: controller.conpasswordController),
+                const SizedBox(height: 10),
+                CommonButton(onTap: () {
+                  if (!controller.formKey.currentState!.validate()) {
+                    return;
+                  } else {
+                    controller.signupFun();
+                    Get.to(const HomeScreen());
+                  }
+                }, buttonName: "Sign Up"),
               ],
             ),
           ),
