64f7d9b313550e066dd6188d73f236e54a5a0b52
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-09 23:20:40 -0800

    first commit

--- lib/view/screen/auth/sign_up.dart
+++ lib/view/screen/auth/sign_up.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:learning_management/controller/ui_controller/auth/sign_up.dart';
+import 'package:learning_management/view/screen/auth/widget/con_password_field.dart';
+import 'package:learning_management/view/screen/auth/widget/email_text_field.dart';
+import 'package:learning_management/view/screen/auth/widget/name_text_field.dart';
+import 'package:learning_management/view/screen/auth/widget/password_text_field.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
@@ -12,7 +16,13 @@ class SignUp extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-
+            NameTextField(nameController: controller.nameController),
+            SizedBox(height: 15),
+            EmailTextField(emailController: controller.emailController),
+            SizedBox(height: 15),
+            PasswordTextField(passwordController: controller.passwordController),
+            SizedBox(height: 15),
+            ConPasswordField(conpasswordController: controller.conpasswordController),
           ],
         ),
       ),
