987a9199d0eea2bb3b2c525e0a59aece1ddf98d2
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-09 23:40:30 -0800

    first commit

--- lib/controller/api_controller/auth/forget_password.dart
+++ lib/controller/api_controller/auth/forget_password.dart
@@ -1,3 +1,15 @@
+import 'dart:developer';
 
+class ForgetPasswordService {
+  static Future forgetpasswordservice() async {
+    try {
 
-class ForgetPasswordService{}
+      await Future.delayed(Duration(seconds: 3));
+      return true;
+
+    } catch (e) {
+      log("error $e");
+    }
+    return false;
+  }
+}
