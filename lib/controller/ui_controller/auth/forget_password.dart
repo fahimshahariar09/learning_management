c41dbba3e19affd2c4241d827d04f2aea7c34d0c
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-09 23:50:17 -0800

    first commit

--- lib/controller/ui_controller/auth/forget_password.dart
+++ lib/controller/ui_controller/auth/forget_password.dart
@@ -1,16 +1,27 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:learning_management/controller/api_controller/auth/forget_password.dart';
 
-class ForgetPasswordController extends GetxController{
-  TextEditingController emailController =TextEditingController();
-  RxBool isLoading =true.obs;
+class ForgetPasswordController extends GetxController {
+  TextEditingController emailController = TextEditingController();
+  RxBool isLoading = true.obs;
 
-  forgetpassFun()async{
-    isLoading.value=true;
+  forgetpassFun() async {
+    isLoading.value = true;
     ForgetPasswordService.forgetpasswordservice(email: emailController.text);
+    isLoading.value=false;
+  }
+
+  @override
+  void onInit() {
+    emailController.text="fahim@gmail.com";
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
   }
 
 }
