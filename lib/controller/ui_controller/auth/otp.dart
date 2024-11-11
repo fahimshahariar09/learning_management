918f8878e65a1982d30e1ad3ec6a18917e284bb0
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-10 23:38:15 -0800

    first commit

--- lib/controller/ui_controller/auth/otp.dart
+++ lib/controller/ui_controller/auth/otp.dart
@@ -8,4 +8,11 @@ TextEditingController numController =TextEditingController();
 TextEditingController num1Controller =TextEditingController();
 TextEditingController num2Controller =TextEditingController();
 TextEditingController num3Controller =TextEditingController();
+RxBool isLoading =true.obs;
+
+otpFun()async{
+  isLoading.value=true;
+  isLoading.value=false;
+}
+
 }
