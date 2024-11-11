91bb02a1280c771c1f56818f49c6ceef63008286
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-10 23:43:35 -0800

    first commit

--- lib/controller/ui_controller/auth/otp.dart
+++ lib/controller/ui_controller/auth/otp.dart
@@ -1,18 +1,30 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:learning_management/controller/api_controller/auth/otp.dart';
 
-class OtpController extends GetxController{
-TextEditingController numController =TextEditingController();
-TextEditingController num1Controller =TextEditingController();
-TextEditingController num2Controller =TextEditingController();
-TextEditingController num3Controller =TextEditingController();
-RxBool isLoading =true.obs;
+class OtpController extends GetxController {
+  TextEditingController numController = TextEditingController();
+  TextEditingController num1Controller = TextEditingController();
+  TextEditingController num2Controller = TextEditingController();
+  TextEditingController num3Controller = TextEditingController();
+  RxBool isLoading = true.obs;
 
-otpFun()async{
-  isLoading.value=true;
-  isLoading.value=false;
-}
+  otpFun() async {
+    isLoading.value = true;
+    OtpService.otpService(
+        num: numController.text,
+        num1: num1Controller.text,
+        num2: num2Controller.text,
+        num3: num3Controller.text);
+    isLoading.value = false;
+  }
 
+  @override
+  void onInit() {
+    numController.text = "2";
+    num1Controller.text = "5";
+    num2Controller.text = "9";
+    num3Controller.text = "0";
+    super.onInit();
+  }
 }
