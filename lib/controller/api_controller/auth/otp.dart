2f680ae291912532c0d094afe48c755fcda04760
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-10 23:35:13 -0800

    first commit

--- lib/controller/api_controller/auth/otp.dart
+++ lib/controller/api_controller/auth/otp.dart
@@ -1,12 +1,14 @@
 import 'dart:developer';
 
 class OtpService {
-  static Future otpService() async {
+  static Future otpService(
+      {required String num,
+      required String num1,
+      required String num2,
+      required String num3}) async {
     try {
-
       await Future.delayed(const Duration(seconds: 3));
       return true;
-
     } catch (e) {
       log("error $e");
     }
--- lib/controller/ui_controller/auth/otp.dart
+++ lib/controller/ui_controller/auth/otp.dart
@@ -5,4 +5,7 @@ import 'package:get/get.dart';
 
 class OtpController extends GetxController{
 TextEditingController numController =TextEditingController();
+TextEditingController num1Controller =TextEditingController();
+TextEditingController num2Controller =TextEditingController();
+TextEditingController num3Controller =TextEditingController();
 }
