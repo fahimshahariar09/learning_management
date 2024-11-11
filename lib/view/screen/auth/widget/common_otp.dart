bd3e5cde48599c895810bf70fbf1f3b96cd8bd19
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-10 23:23:08 -0800

    first commit

---
+++ lib/view/screen/auth/widget/common_otp.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class CommonOtp extends StatelessWidget {
+  const CommonOtp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
