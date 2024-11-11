67a9a4db4678fecfa3fe98d1c10a913161c700ad
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-10 23:18:39 -0800

    first commit

--- lib/controller/api_controller/auth/otp.dart
+++ lib/controller/api_controller/auth/otp.dart
@@ -1,3 +1,15 @@
+import 'dart:developer';
 
+class OtpService {
+  static Future otpService() async {
+    try {
 
-class OtpService{}
+      await Future.delayed(const Duration(seconds: 3));
+      return true;
+
+    } catch (e) {
+      log("error $e");
+    }
+    return false;
+  }
+}
