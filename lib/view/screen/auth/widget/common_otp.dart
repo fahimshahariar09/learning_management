be70e9528c128e7c0ee36daff733a4d79b027849
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-10 23:31:10 -0800

    first commit

--- lib/view/screen/auth/widget/common_otp.dart
+++ lib/view/screen/auth/widget/common_otp.dart
@@ -1,20 +1,24 @@
-
 import 'package:flutter/material.dart';
 
 class CommonOtp extends StatelessWidget {
-  const CommonOtp({super.key});
+  const CommonOtp({super.key, required this.otpController});
+
+  final TextEditingController otpController;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5)
+    return SizedBox(
+      height: 40,
+      width: 50,
+      child: TextField(
+        controller: otpController,
+        decoration: InputDecoration(
+          focusedBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
+          enabledBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
+          fillColor: Colors.grey,
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5)
-        ),
-        fillColor: Colors.grey,
       ),
     );
   }
