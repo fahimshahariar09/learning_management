53c5415820600a681ef665aeda051085d94e98a5
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-11 23:31:09 -0800

    first commit

--- lib/controller/ui_controller/auth/new_password.dart
+++ lib/controller/ui_controller/auth/new_password.dart
@@ -2,4 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class NewPasswordController extends GetxController{
+  TextEditingController oldpassword =TextEditingController();
+  TextEditingController newpassword =TextEditingController();
+  TextEditingController conpassword =TextEditingController();
+  RxBool isLoading=true.obs;
 }
