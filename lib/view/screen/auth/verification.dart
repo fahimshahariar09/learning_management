80ef065c4e143c16f7f9e9b704758e5868b3edfc
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-11 23:38:52 -0800

    first commit

--- lib/view/screen/auth/verification.dart
+++ lib/view/screen/auth/verification.dart
@@ -5,6 +5,17 @@ class Verification extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
