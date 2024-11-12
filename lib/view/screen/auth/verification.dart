c2ca1c019e5ef10c6242b751ac46553fa2871dc1
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-11 23:40:27 -0800

    first commit

--- lib/view/screen/auth/verification.dart
+++ lib/view/screen/auth/verification.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning_management/controller/ui_controller/auth/otp.dart';
+import 'package:learning_management/view/screen/auth/widget/common_otp.dart';
 
 class Verification extends StatelessWidget {
   const Verification({super.key});
 
   @override
   Widget build(BuildContext context) {
+    OtpController otpController = Get.put(OtpController());
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15),
           child: Column(
             children: [
-
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CommonOtp(otpController: otpController.numController),
+                ],
+              )
             ],
           ),
         ),
