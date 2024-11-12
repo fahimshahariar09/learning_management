370aaf5968f58920d31d2737571a093d434cf574
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-11 23:28:01 -0800

    first commit

--- lib/controller/api_controller/auth/new_password.dart
+++ lib/controller/api_controller/auth/new_password.dart
@@ -1,8 +1,16 @@
 import 'dart:developer';
 
 class NewPasswordService {
-  static Future newpasswordService() async {
-    try {} catch (e) {
+  static Future newpasswordService({
+    required String oldpassword,
+    required String newpassword,
+    required String conpassword,
+  }) async {
+    try {
+
+      await Future.delayed(const Duration(seconds: 3));
+      return true;
+    } catch (e) {
       log("error $e");
     }
     return false;
