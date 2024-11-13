0f4d5cd74393e03472a98e2b233e50fdd4cd2c29
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-12 23:38:24 -0800

    first commit

--- lib/controller/ui_controller/bottom_navi_bar/bottom_navi_bar.dart
+++ lib/controller/ui_controller/bottom_navi_bar/bottom_navi_bar.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class BottomNaviBarController extends GetxController{}
+class BottomNaviBarController extends GetxController{
+  final pageController = PageController(initialPage:0);
+}
