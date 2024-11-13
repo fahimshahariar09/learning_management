e763a855beac4233dc8f98c2d5d148dfc7b5cc0f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-12 23:40:29 -0800

    first commit

--- lib/controller/ui_controller/bottom_navi_bar/bottom_navi_bar.dart
+++ lib/controller/ui_controller/bottom_navi_bar/bottom_navi_bar.dart
@@ -1,6 +1,10 @@
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class BottomNaviBarController extends GetxController{
   final pageController = PageController(initialPage:0);
+  final NotchBottomBarController nController = NotchBottomBarController(index: 0);
+  RxInt index = 0.obs;
+  RxInt maxCount =3.obs;
 }
