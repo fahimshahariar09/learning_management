5861f3810b47ab5973460e7d22614b6558989d4a
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-12 23:29:23 -0800

    first commit

--- lib/view/screen/auth/new_password.dart
+++ lib/view/screen/auth/new_password.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning_management/controller/ui_controller/auth/new_password.dart';
+import 'package:learning_management/view/screen/auth/widget/password_text_field.dart';
 
 class NewPassword extends StatelessWidget {
   const NewPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
+    NewPasswordController controller =Get.put(NewPasswordController());
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15),
           child: Column(
             children: [
+              PasswordTextField(passwordController: controller.oldpassword),
+              SizedBox(height: 10),
+              PasswordTextField(passwordController: controller.newpassword),
+              SizedBox(height: 10),
+              PasswordTextField(passwordController: controller.conpassword),
+              SizedBox(height: 10),
 
             ],
           ),
