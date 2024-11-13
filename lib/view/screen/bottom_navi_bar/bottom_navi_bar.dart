8a88e1068e81019d7cd4ebe8f1901c0e0319cdf4
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-12 23:52:31 -0800

    first commit

--- lib/view/screen/bottom_navi_bar/bottom_navi_bar.dart
+++ lib/view/screen/bottom_navi_bar/bottom_navi_bar.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning_management/controller/ui_controller/bottom_navi_bar/bottom_navi_bar.dart';
 
 class BottomNaviBar extends StatelessWidget {
   const BottomNaviBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    BottomNaviBarController bottomNaviBarController =
+        Get.put(BottomNaviBarController());
+    return Scaffold(
+      body: PageView(
+        controller: bottomNaviBarController.pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: List.generate(bottomNaviBarController.bottomBarPage.length,
+            (index) => bottomNaviBarController.bottomBarPage[index]),
+      ),
+    );
   }
 }
