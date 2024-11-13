21b19648f1c7e12255f4419b12633ceb14e1ef1f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-12 23:49:08 -0800

    first commit

---
+++ lib/view/screen/bottom_navi_bar/bottom_navi_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BottomNaviBar extends StatelessWidget {
+  const BottomNaviBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
