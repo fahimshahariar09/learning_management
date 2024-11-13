425f10ac5d59a96c36a768dfd74638b9c315eecd
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-12 23:45:44 -0800

    first commit

--- lib/controller/ui_controller/bottom_navi_bar/bottom_navi_bar.dart
+++ lib/controller/ui_controller/bottom_navi_bar/bottom_navi_bar.dart
@@ -1,10 +1,23 @@
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:learning_management/view/screen/home_screen/home_screen.dart';
 
 class BottomNaviBarController extends GetxController{
   final pageController = PageController(initialPage:0);
   final NotchBottomBarController nController = NotchBottomBarController(index: 0);
   RxInt index = 0.obs;
   RxInt maxCount =3.obs;
+  RxInt page =0.obs;
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  RxList<Widget> bottomBarPage =[
+    HomeScreen(),
+  ].obs;
+
 }
