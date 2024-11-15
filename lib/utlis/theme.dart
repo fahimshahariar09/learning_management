c2410df7cb2d8d7d4cd71518e8b53bc5a4c433d2
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-14 23:15:24 -0800

    first commit

--- lib/utlis/theme.dart
+++ lib/utlis/theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class AppTheme{
   ThemeData lightTheme = ThemeData(
-    appBarTheme: AppBarTheme(
+    appBarTheme: const AppBarTheme(
       backgroundColor: Colors.blue,
       centerTitle: true,
       titleTextStyle: TextStyle(
@@ -15,7 +15,7 @@ class AppTheme{
   );
 
   ThemeData darkTheme = ThemeData(
-    appBarTheme: AppBarTheme(
+    appBarTheme: const AppBarTheme(
       centerTitle: true,
       titleTextStyle: TextStyle(
         color: Colors.black,
