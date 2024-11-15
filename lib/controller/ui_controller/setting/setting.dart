0714eae9f98d4a0a60cdb428de5a1c7f621601c5
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-14 23:26:36 -0800

    first commit

--- lib/controller/ui_controller/setting/setting.dart
+++ lib/controller/ui_controller/setting/setting.dart
@@ -1,13 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SettingController extends GetxController{
-  RxBool isLightTheme =true.obs;
+class SettingController extends GetxController {
+  RxBool isLightTheme = true.obs;
 
-  changeThemeFun()async{
-    isLightTheme.value =!isLightTheme.value;
-    Get.changeTheme(!isLightTheme.value? ThemeData.dark() : ThemeData.light());
+  changeThemeFun() async {
+    isLightTheme.value = !isLightTheme.value;
+    Get.changeTheme(!isLightTheme.value ? ThemeData.dark() : ThemeData.light());
   }
-
 }
--- lib/view/setting/setting.dart
+++ lib/view/setting/setting.dart
@@ -1,10 +1,17 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning_management/controller/ui_controller/setting/setting.dart';
 
 class Setting extends StatelessWidget {
-    const Setting({super.key});
+  const Setting({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold();
-    }
+  @override
+  Widget build(BuildContext context) {
+    SettingController controller = Get.put(SettingController());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Setting"),
+      ),
+    );
   }
+}
