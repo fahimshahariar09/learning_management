603b44a0de38236cb2f81757b1260295d27dc884
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-15 23:48:21 -0800

    first commit

--- lib/controller/ui_controller/setting/setting.dart
+++ lib/controller/ui_controller/setting/setting.dart
@@ -5,10 +5,19 @@ class SettingController extends GetxController {
   RxBool isLightTheme = true.obs;
 
   RxString selectedLanguage ="BD".obs;
-  RxList<String> languageList =<String>[].obs;
+  RxList<String> languageList =<String>[
+    "BD",
+    "US",
+    "India",
+  ].obs;
 
   changeThemeFun() async {
     isLightTheme.value = !isLightTheme.value;
     Get.changeTheme(!isLightTheme.value ? ThemeData.dark() : ThemeData.light());
   }
+
+  changeLanguageFun(){
+    String local ="en";
+  }
+
 }
