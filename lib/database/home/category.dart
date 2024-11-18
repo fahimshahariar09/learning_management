e6fdeb1a4c76b71647c212984c666063265d0a32
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-17 23:45:56 -0800

    first commit

--- lib/database/home/category.dart
+++ lib/database/home/category.dart
@@ -1,7 +1,7 @@
 class CategoryList {
   static Map<String, dynamic> categorylist = {
     'category': [
-      {"images": "asset/icon/category.png", "name": "category"},
+      {"images": "asset/icon/category.png", "name": "Category"},
       {"images": "asset/icon/courses.png", "name": "Courses"},
       {"images": "asset/icon/reword.png", "name": "Reward"},
       {"images": "asset/icon/free_courses.png", "name": "Free Courses"},
