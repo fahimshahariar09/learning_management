4b1445ab71541e8d3dcfc42edf08c0ee817687fc
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-17 23:43:24 -0800

    first commit

--- lib/database/home/category.dart
+++ lib/database/home/category.dart
@@ -4,9 +4,9 @@ class CategoryList {
       {"images": "asset/icon/category.png", "name": "category"},
       {"images": "asset/icon/courses.png", "name": "Courses"},
       {"images": "asset/icon/reword.png", "name": "Reward"},
-      {"images": "asset/image/splash.png", "name": "Free Courses"},
-      {"images": "asset/image/splash.png", "name": "BookStore"},
-      {"images": "asset/image/splash.png", "name": "Quiz"}
+      {"images": "asset/icon/free_courses.png", "name": "Free Courses"},
+      {"images": "asset/icon/book.png", "name": "BookStore"},
+      {"images": "asset/icon/quiz.png", "name": "Quiz"}
     ]
   };
 }
