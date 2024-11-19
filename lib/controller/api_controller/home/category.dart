df87a9d4896d3bdebf73976d04504c9fb270a6ce
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-18 23:25:03 -0800

    first commit

--- lib/controller/api_controller/home/category.dart
+++ lib/controller/api_controller/home/category.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
 import 'dart:developer';
 
+import 'package:learning_management/model/home/category_model.dart';
+
 class CategoryService {
-  static Future categoryService() async {
-    try {} catch (e) {
+  static Future<CategoryModel?> categoryService() async {
+    try {
+      CategoryModel model =CategoryModel.fromJson(jsonDecode(jsonEncode()))
+    } catch (e) {
       log("error $e");
     }
+    return null;
   }
 }
--- lib/controller/ui_controller/home/home.dart
+++ lib/controller/ui_controller/home/home.dart
@@ -1,7 +1,10 @@
 import 'package:get/get.dart';
+import 'package:learning_management/database/home/category.dart';
 
 class HomeController extends GetxController {
   RxBool isLoading = true.obs;
+
+  RxList<CategoryList> category =<CategoryList>[].obs;
 
   categoryFun() async {
     isLoading.value = true;
