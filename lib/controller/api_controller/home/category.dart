ef3a11ebbe55d2bb5f320382da590ca1b7ff7c07
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-19 23:41:31 -0800

    first commit

--- lib/controller/api_controller/home/category.dart
+++ lib/controller/api_controller/home/category.dart
@@ -6,8 +6,6 @@ import 'package:learning_management/model/home/category_model.dart';
 class CategoryService {
   static Future<CategoryModel?> categoryService() async {
     try {
-      CategoryModel model =CategoryModel.fromJson(jsonDecode(jsonEncode(category)));
-      return model;
     } catch (e) {
       log("error $e");
     }
--- lib/controller/api_controller/home/courses.dart
+++ lib/controller/api_controller/home/courses.dart
@@ -4,12 +4,13 @@ import 'package:learning_management/database/home/courses.dart';
 import 'package:learning_management/model/home/courses_model.dart';
 
 class CoursesService{
-  static Future coursesService()async{
+  static Future<List<CoursesModel>> coursesService()async{
     try{
 
       List<CoursesModel> data =[];
       for(var i in CoursesList.courseslist[""].toList()){}
 
     }catch(e){log("error $e");}
+  return [];
   }
 }
