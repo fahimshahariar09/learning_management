084909380d7112d08bea54c72f2fdbe53950f87c
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-20 23:26:47 -0800

    first commit

--- lib/model/home/category_model.dart
+++ lib/model/home/category_model.dart
@@ -1,26 +1,3 @@
-class CategoryModel {
-  List<CategoryList>? categorylist;
-
-  CategoryModel({this.categorylist});
-
-  CategoryModel.fromJson(Map<String, dynamic> json) {
-    if (json['category'] != null) {
-      categorylist = <CategoryList>[];
-      json['category'].forEach((v) {
-        categorylist!.add(CategoryList.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    if (categorylist != null) {
-      data['category'] = categorylist!.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
-}
-
 class CategoryList {
   String? images;
   String? name;
@@ -30,12 +7,5 @@ class CategoryList {
   CategoryList.fromJson(Map<String, dynamic> json) {
     images = json['images'];
     name = json['name'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['images'] = images;
-    data['name'] = name;
-    return data;
   }
 }
