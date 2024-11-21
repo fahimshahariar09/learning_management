a76db8e1f39e5bfd5920adb799e9972f6fad08b0
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-20 23:22:39 -0800

    first commit

--- lib/controller/ui_controller/home/home.dart
+++ lib/controller/ui_controller/home/home.dart
@@ -1,10 +1,15 @@
 import 'package:get/get.dart';
 import 'package:learning_management/database/home/category.dart';
+import 'package:learning_management/model/home/courses_model.dart';
 
 class HomeController extends GetxController {
   RxBool isLoading = true.obs;
 
   RxList<CategoryList> category =<CategoryList>[].obs;
+  List<CoursesModel> coursesList=[];
+
+
+  coursesFun()async{}
 
   categoryFun() async {
     isLoading.value = true;
