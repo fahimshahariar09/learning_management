cd66f10b88a11db096e27c683d54a2cc012f6909
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-21 23:30:40 -0800

    first commit

--- lib/controller/ui_controller/home/widget/home_card.dart
+++ lib/controller/ui_controller/home/widget/home_card.dart
@@ -6,13 +6,12 @@ class HomeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: SizedBox(
+      child: Container(
         height: 100,
         width: 150,
-        child: Column(
-          children: [
-
-          ],
+        decoration: const BoxDecoration(color: Colors.red),
+        child: const Column(
+          children: [],
         ),
       ),
     );
