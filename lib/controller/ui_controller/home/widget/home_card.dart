de6745478fad4497677372b78057b9d5ec342b62
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-21 23:26:03 -0800

    first commit

--- lib/controller/ui_controller/home/widget/home_card.dart
+++ lib/controller/ui_controller/home/widget/home_card.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/material.dart';
+
 class HomeCard extends StatelessWidget {
   const HomeCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      child: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
