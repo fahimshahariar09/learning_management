1daf626603dd5f955ddf31e3ae39317c54c0a2a9
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-22 23:31:08 -0800

    first commit

--- lib/controller/ui_controller/home/home.dart
+++ lib/controller/ui_controller/home/home.dart
@@ -1,7 +1,6 @@
 import 'package:get/get.dart';
 import 'package:learning_management/controller/api_controller/home/category.dart';
 import 'package:learning_management/controller/api_controller/home/courses.dart';
-import 'package:learning_management/database/home/category.dart';
 import 'package:learning_management/model/home/category_model.dart';
 import 'package:learning_management/model/home/courses_model.dart';
 
--- lib/controller/ui_controller/splash/splash.dart
+++ lib/controller/ui_controller/splash/splash.dart
@@ -1,15 +1,13 @@
-
 import 'package:get/get.dart';
 
-class SplashScreenController extends GetxController{
-
-  Future nextPage() async{
+class SplashScreenController extends GetxController {
+  Future nextPage() async {
     await Future.delayed(const Duration(seconds: 3));
   }
 
   @override
   void onInit() {
+    nextPage();
     super.onInit();
   }
-
 }
