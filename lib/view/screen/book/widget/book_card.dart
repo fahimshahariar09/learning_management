21d5c6382c4bb3e70f3887d1d3220ca483f1dc9f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-23 23:45:44 -0800

    first commit

--- lib/view/screen/book/widget/book_card.dart
+++ lib/view/screen/book/widget/book_card.dart
@@ -5,6 +5,18 @@ class BookCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Card(
+      child: Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            children: [
+
+            ],
+          )),
+    );
   }
 }
