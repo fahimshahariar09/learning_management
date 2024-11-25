da1f22092a7c352cc1e6a4a3b8d34c323296b607
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-24 22:37:59 -0800

    first commit

--- lib/database/mentor/top_mentor_list.dart
+++ lib/database/mentor/top_mentor_list.dart
@@ -1,11 +1,9 @@
-class TopMentorList{
-  static Map<String,dynamic> tomentorList ={
-    "booklist" : [
-      {
-        "image" :"",
-        "name" :"",
-        "titel" :""
-      }
+class TopMentorList {
+  static Map<String, dynamic> tomentorList = {
+    "booklist": [
+      {"image": "", "name": "Lisa Rahman", "titel": "UI/UX Design"},
+      {"image": "", "name": "Charli Fallon", "titel": "React"},
+      {"image": "", "name": "Jennifer", "titel": "Java"},
     ]
   };
 }
