5df68ffa6d43b65fcd70921fa73887a1e545b670
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-24 22:22:25 -0800

    first commit

--- lib/model/book_list.dart
+++ lib/model/book_list.dart
@@ -2,4 +2,9 @@ class BookListModel{
   String? image;
   String? name;
   String? titel;
+
+  BookListModel({this.image,this.name,this.titel});
+
+  BookListModel.formJson(Map<String,dynamic> json){}
+
 }
