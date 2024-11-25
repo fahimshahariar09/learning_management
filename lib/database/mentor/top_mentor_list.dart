558bfba5e9f48adfd6cf88371ac6295cb843d50a
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-11-24 22:39:45 -0800

    first commit

--- lib/database/mentor/top_mentor_list.dart
+++ lib/database/mentor/top_mentor_list.dart
@@ -4,6 +4,9 @@ class TopMentorList {
       {"image": "", "name": "Lisa Rahman", "titel": "UI/UX Design"},
       {"image": "", "name": "Charli Fallon", "titel": "React"},
       {"image": "", "name": "Jennifer", "titel": "Java"},
+      {"image": "", "name": "Susan James", "titel": "Python"},
+      {"image": "", "name": "Jennifer", "Rifat": "Flutter"},
+      {"image": "", "name": "Jennifer", "Edward Odell": "Node js"},
     ]
   };
 }
