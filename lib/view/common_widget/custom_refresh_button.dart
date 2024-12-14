c2f2ea27336dc357cc5803ea83b7177ee6e91060
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-13 23:26:37 -0800

    first commit

--- lib/view/common_widget/custom_refresh_button.dart
+++ lib/view/common_widget/custom_refresh_button.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 class CustomRefreshButton extends StatelessWidget {
-  const CustomRefreshButton({super.key});
+  const CustomRefreshButton(
+      {super.key,
+      required this.refreshButton,
+      this.height,
+      this.width,
+      this.containerColor,
+      this.iconColor});
+
+  final VoidCallback refreshButton;
+  final double? height;
+  final double? width;
+  final Color? containerColor;
+  final Color? iconColor;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      height: 60,
+      width: MediaQuery.sizeOf(context).width,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
   }
 }
