e03b119b69255e69a08d4e4047bb319c5434533d
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-13 23:38:50 -0800

    first commit

--- lib/view/common_widget/image_view_widget.dart
+++ lib/view/common_widget/image_view_widget.dart
@@ -5,6 +5,14 @@ class ImageViewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          height: MediaQuery.sizeOf(context).height,
+          width:  MediaQuery.sizeOf(context).width,
+          child: Hero(tag: "das", child: PhotoView()),
+        ),
+      ),
+    );
   }
 }
