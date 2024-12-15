ccc6e1a6c1d40fdaaa9f20bc26f937f0df2d9a8e
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-14 23:51:50 -0800

    first commit

--- lib/controller/ui_controller/profile.dart
+++ lib/controller/ui_controller/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController{
+  final formKey = GlobalKey<FormState>();
   TextEditingController nameController = TextEditingController();
   TextEditingController mailController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
@@ -9,4 +10,9 @@ class ProfileController extends GetxController{
   TextEditingController oldController = TextEditingController();
   TextEditingController newController = TextEditingController();
   TextEditingController confController = TextEditingController();
+  RxInt selectedTab =0.obs;
+  RxInt selectedOrderTab =1.obs;
+  RxBool languageBN = true.obs;
+  RxBool themeLight = true.obs;
+  RxBool isLoading = false.obs;
 }
