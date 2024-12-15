17a5f97390758fa7cdfe6fab95d6afa394f1c70f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-14 23:47:17 -0800

    first commit

--- lib/controller/ui_controller/profile.dart
+++ lib/controller/ui_controller/profile.dart
@@ -1,3 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class ProfileController extends GetxController{}
+class ProfileController extends GetxController{
+  TextEditingController nameController = TextEditingController();
+  TextEditingController mailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+  TextEditingController oldController = TextEditingController();
+  TextEditingController newController = TextEditingController();
+  TextEditingController confController = TextEditingController();
+}
