597cf900349e4ec924a5d8f282a649c07fee96a5
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-14 23:40:34 -0800

    first commit

--- lib/controller/api_controller/profile_update.dart
+++ lib/controller/api_controller/profile_update.dart
@@ -7,7 +7,18 @@ class ProfileUpdateService {
       {required String name,
       required String address,
       required File image}) async {
-    try {} catch (e) {
+    try {
+      await Future.delayed(const Duration(seconds: 2));
+      var data = {
+        "id": 4,
+        "name": "fahim",
+        "username": "null",
+        "email": "fahim@gmail.com",
+        "address": "google",
+        "profile_image": "url",
+        "data": "14 may 2024"
+      };
+    } catch (e) {
       debugPrint("error$e");
     }
     return false;
