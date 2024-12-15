e279f2647d181f638bf79c1760382a357b2b8a91
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-14 23:21:16 -0800

    first commit

--- lib/view/common_widget/image_view_widget.dart
+++ lib/view/common_widget/image_view_widget.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 
 class ImageViewWidget extends StatelessWidget {
-  const ImageViewWidget({super.key});
+  const ImageViewWidget({super.key, required this.imageUrl});
+
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +12,12 @@ class ImageViewWidget extends StatelessWidget {
       body: SafeArea(
         child: SizedBox(
           height: MediaQuery.sizeOf(context).height,
-          width:  MediaQuery.sizeOf(context).width,
-          child: Hero(tag: "das", child: PhotoView()),
+          width: MediaQuery.sizeOf(context).width,
+          child: Hero(
+              tag: "das",
+              child: PhotoView(
+                imageProvider: NetworkImage(imageUrl),
+              )),
         ),
       ),
     );
