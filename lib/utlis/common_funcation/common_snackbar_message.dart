d9e7dbc1e29f6b80de6e3783a3f8c8f5bb9e2ade
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-15 23:26:47 -0800

    first commit

--- lib/utlis/common_funcation/common_snackbar_message.dart
+++ lib/utlis/common_funcation/common_snackbar_message.dart
@@ -1,1 +1,16 @@
-class CommonSnackBarMessage{}
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CommonSnackBarMessage{
+  static noInternetConnection(){
+    Get.snackbar("you a`re offline !!", "please check internet connection",
+    backgroundColor: Colors.red,
+      colorText: Colors.redAccent,
+      snackStyle: SnackStyle.FLOATING,
+      duration: const Duration(seconds: 2),
+      overlayBlur: 5,
+      icon: const Icon(Icons.wifi_off_outlined,color: Colors.red,),
+      snackPosition: SnackPosition.TOP
+    );
+  }
+}
