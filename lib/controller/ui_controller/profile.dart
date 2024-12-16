42f76eb82f44cb0653bbd769981f3f061e702764
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-15 23:18:40 -0800

    first commit

--- lib/controller/ui_controller/profile.dart
+++ lib/controller/ui_controller/profile.dart
@@ -5,8 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:learning_management/controller/local_storage/local_storage.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class ProfileController extends GetxController{
+class ProfileController extends GetxController {
   final formKey = GlobalKey<FormState>();
   TextEditingController nameController = TextEditingController();
   TextEditingController mailController = TextEditingController();
@@ -15,22 +16,39 @@ class ProfileController extends GetxController{
   TextEditingController oldController = TextEditingController();
   TextEditingController newController = TextEditingController();
   TextEditingController confController = TextEditingController();
-  RxInt selectedTab =0.obs;
-  RxInt selectedOrderTab =1.obs;
+  RxInt selectedTab = 0.obs;
+  RxInt selectedOrderTab = 1.obs;
   RxBool languageBN = true.obs;
   RxBool themeLight = true.obs;
   RxBool isLoading = false.obs;
-  RxMap<dynamic,dynamic> userInfo = {}.obs;
-  ImageSource imageSource =ImageSource.camera;
-  late Rx<File?>  profileIMG = Rx<File?>(null);
-  final picker =ImagePicker();
+  RxMap<dynamic, dynamic> userInfo = {}.obs;
+  ImageSource imageSource = ImageSource.camera;
+  late Rx<File?> profileIMG = Rx<File?>(null);
+  final picker = ImagePicker();
 
-  getProfileInfo()async{
+  @override
+  void onInit() {
+    getProfileInfo();
+    super.onInit();
+  }
+
+  getProfileInfo() async {
     var a = await LocalData().readData(key: "userInfo");
-    if(a!=""){
+    if (a != "") {
       userInfo.value = jsonDecode(a);
       nameController.text = userInfo["name"] ?? "";
       addressController.text = userInfo["address"] ?? "";
+    }
+  }
+
+  getImage()async{
+    var status = await Permission.camera.request();
+    if(status.isGranted){
+      final pickedFile = await picker.pickImage(source: imageSource,imageQuality: 25);
+      if(pickedFile != null){
+        profileIMG.value = File(pickedFile.path);
+      }
+    }else{
     }
   }
 
