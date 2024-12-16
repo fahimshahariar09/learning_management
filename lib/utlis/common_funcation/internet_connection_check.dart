5a89fc99561b186d6b181c8376d6f06c6a8165b3
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-15 23:37:25 -0800

    first commit

--- lib/utlis/common_funcation/internet_connection_check.dart
+++ lib/utlis/common_funcation/internet_connection_check.dart
@@ -1,2 +1,10 @@
-class ConnectionChecker{
+import 'package:flutter/cupertino.dart';
+
+class ConnectionChecker {
+  static Future checkConnection() async {
+    try {} catch (e) {
+      debugPrint("error $e");
+    }
+    return false;
+  }
 }
