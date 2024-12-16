745cac6c3ca9a6f440e5566f47c4a2c7bbffa4b5
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-15 23:44:46 -0800

    first commit

---
+++ lib/view/screen/profile/widget/log_out.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class LogOut extends StatelessWidget {
+  const LogOut({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: child);
+  }
+}
