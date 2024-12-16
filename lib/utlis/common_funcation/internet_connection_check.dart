1095ad396f333fa60aa927afe25c59be8acd6e5f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-15 23:40:41 -0800

    first commit

--- lib/utlis/common_funcation/internet_connection_check.dart
+++ lib/utlis/common_funcation/internet_connection_check.dart
@@ -1,10 +1,19 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 
 class ConnectionChecker {
   static Future checkConnection() async {
-    try {} catch (e) {
+    try {
+      final connectivityResult = await Connectivity().checkConnectivity();
+      if(connectivityResult[0] == ConnectivityResult.none){
+        return false;
+      }else{
+        return true;
+      }
+    } on PlatformException catch (e) {
       debugPrint("error $e");
+      return false;
     }
-    return false;
   }
 }
