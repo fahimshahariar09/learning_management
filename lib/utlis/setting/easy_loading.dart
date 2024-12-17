3668dd531a38299de21e5aaf293a8528c1b011de
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-16 23:20:18 -0800

    first commit

--- lib/utlis/setting/easy_loading.dart
+++ lib/utlis/setting/easy_loading.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:learning_management/utlis/app_colors.dart';
 
 class EasyloadingSettings {
   easyloadingSetting() {
     EasyLoading.instance
       ..displayDuration = const Duration(milliseconds: 500)
       ..loadingStyle = EasyLoadingStyle.custom
-      ..backgroundColor = Colors.red
-      ..indicatorColor = Colors.red
-      ..maskColor = Colors.red
-      ..textColor = Colors.red
+      ..backgroundColor = AppColors.bg1LightColor
+      ..indicatorColor = AppColors.bg1LightColor
+      ..maskColor = AppColors.bg1LightColor
+      ..textColor = AppColors.bg1LightColor
       ..contentPadding = const EdgeInsets.symmetric(horizontal: 50, vertical: 20)
       ..indicatorType = EasyLoadingIndicatorType.fadingGrid
       ..userInteractions = false
