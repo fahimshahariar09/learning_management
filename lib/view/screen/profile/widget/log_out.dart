7dc42c9163a4fe1796cff07ebd5a80a97d32cc77
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-16 23:22:47 -0800

    first commit

--- lib/view/screen/profile/widget/log_out.dart
+++ lib/view/screen/profile/widget/log_out.dart
@@ -1,10 +1,19 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LogOut extends StatelessWidget {
-  const LogOut({super.key});
+  const LogOut({super.key, required this.onTap});
+
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: child);
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        elevation: 4,
+        surfaceTintColor: Colors.transparent,
+        shape: OutlineInputBorder(borderSide: BorderSide.none),
+      ),
+    );
   }
 }
