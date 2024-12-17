35d318f12e5bf3e8d0a1405beb5db6eaab7676e2
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-16 23:38:22 -0800

    first commit

--- lib/view/screen/profile/widget/custom_text_field.dart
+++ lib/view/screen/profile/widget/custom_text_field.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:learning_management/utlis/app_colors.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.controller, this.textInputType});
+  const CustomTextField(
+      {super.key, required this.controller, this.textInputType});
 
   final TextEditingController controller;
   final TextInputType? textInputType;
@@ -9,14 +11,20 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 7,horizontal: 5),
+      padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 5),
       child: TextFormField(
         controller: controller,
         keyboardType: textInputType ?? TextInputType.text,
-        decoration: InputDecoration(
-          focusedBorder: OutlineInputBorder(),
+        decoration: const InputDecoration(
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.bg1LightColor)),
+          border: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.cardDarkColor)),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.cardDarkColor)),
+          errorBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.cardDarkColor)),
         ),
-
       ),
     );
   }
