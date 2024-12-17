d3088d3ad09a29f77382c5a0ea6519acc28615f7
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-16 23:31:27 -0800

    first commit

--- lib/view/screen/profile/widget/change_password_section.dart
+++ lib/view/screen/profile/widget/change_password_section.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:learning_management/controller/ui_controller/profile.dart';
+import 'package:learning_management/utlis/app_colors.dart';
+import 'package:learning_management/view/common_widget/custom_text_widget.dart';
 
 class ChangePasswordSection extends StatelessWidget {
   const ChangePasswordSection({super.key});
@@ -9,12 +11,29 @@ class ChangePasswordSection extends StatelessWidget {
   Widget build(BuildContext context) {
     ProfileController profileController = Get.put(ProfileController());
     return Padding(
-      padding: const EdgeInsets.only(left: 10,right: 10,top: 10,bottom: 10),
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
       child: SingleChildScrollView(
         child: Form(
           key: profileController.formKey,
-          child: Column(
-            children: [],
+          child: const Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.vpn_key,
+                    color: AppColors.bg1LightColor,
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextWidget(
+                    text: "change_password",
+                    fontSize: 22,
+                    fontColor: AppColors.bg1LightColor,
+                    fontWeight: FontWeight.w500,
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
