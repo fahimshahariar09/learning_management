f3cbf996c4f410fff9fab0f66efb943f29eac082
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-16 23:36:06 -0800

    first commit

--- lib/view/screen/profile/widget/custom_text_field.dart
+++ lib/view/screen/profile/widget/custom_text_field.dart
@@ -1,10 +1,23 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.controller, this.textInputType});
+
+  final TextEditingController controller;
+  final TextInputType? textInputType;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 7,horizontal: 5),
+      child: TextFormField(
+        controller: controller,
+        keyboardType: textInputType ?? TextInputType.text,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(),
+        ),
+
+      ),
+    );
   }
 }
