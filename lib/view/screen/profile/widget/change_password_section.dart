e2a7918f193bb39ef902a66125f8baf061345f98
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-16 23:27:30 -0800

    first commit

---
+++ lib/view/screen/profile/widget/change_password_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ChangePasswordSection extends StatelessWidget {
+  const ChangePasswordSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
