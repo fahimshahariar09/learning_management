860f98fff74d35e32cc59a776466460399000a0c
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-17 23:40:36 -0800

    first commit

--- lib/view/screen/profile/widget/deleted_account_section.dart
+++ lib/view/screen/profile/widget/deleted_account_section.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:learning_management/controller/ui_controller/profile.dart';
 
 class DeletedAccountSection extends StatelessWidget {
   const DeletedAccountSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    ProfileController profileController =Get.put(ProfileController());
+    return Padding(
+      padding: const EdgeInsets.only(top: 20,right: 10,left: 10,bottom: 10),
+      child: SingleChildScrollView(
+        child: Form(
+          key: profileController.formKey,
+          child: Column(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
