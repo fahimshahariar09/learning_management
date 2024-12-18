4b692aacf9e5bccd6dc8dea224fba644ace66c9d
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-17 23:21:53 -0800

    first commit

--- lib/view/screen/profile/widget/change_password_section.dart
+++ lib/view/screen/profile/widget/change_password_section.dart
@@ -1,7 +1,11 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:learning_management/controller/ui_controller/profile.dart';
 import 'package:learning_management/utlis/app_colors.dart';
+import 'package:learning_management/utlis/common_funcation/common_snackbar_message.dart';
+import 'package:learning_management/utlis/common_funcation/internet_connection_check.dart';
+import 'package:learning_management/view/common_widget/custom_button.dart';
 import 'package:learning_management/view/common_widget/custom_text_widget.dart';
 import 'package:learning_management/view/screen/profile/widget/custom_text_field.dart';
 
@@ -36,17 +40,28 @@ class ChangePasswordSection extends StatelessWidget {
               ),
               const SizedBox(height: 10),
               CustomTextField(
-                controller: profileController.oldController,
+                controller: profileController.oldPasswordController,
                 labelText: 'current_password',
               ),
               CustomTextField(
-                controller: profileController.oldController,
+                controller: profileController.newPasswordController,
                 labelText: 'new_password',
               ),
               CustomTextField(
-                controller: profileController.confController,
+                controller: profileController.confPasswordController,
                 labelText: 'conf_password',
               ),
+              SizedBox(height: 10),
+              CustomButton(onTap: ()async{
+                FocusScope.of(context).unfocus();
+                if(!profileController.formKey.currentState!.validate()){
+                  return ;
+                }
+                if(!await ConnectionChecker.checkConnection()){
+                  CommonSnackBarMessage.noInternetConnection();
+                  return ;
+                }
+              }, text: "edit")
             ],
           ),
         ),
