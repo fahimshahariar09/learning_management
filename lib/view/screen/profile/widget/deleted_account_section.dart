43d836bc9dcd547150f43342b2fe784638ae71a5
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-17 23:43:33 -0800

    first commit

--- lib/view/screen/profile/widget/deleted_account_section.dart
+++ lib/view/screen/profile/widget/deleted_account_section.dart
@@ -1,21 +1,36 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:learning_management/controller/ui_controller/profile.dart';
+import 'package:learning_management/utlis/app_colors.dart';
+import 'package:learning_management/view/screen/profile/widget/custom_text_field.dart';
 
 class DeletedAccountSection extends StatelessWidget {
   const DeletedAccountSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    ProfileController profileController =Get.put(ProfileController());
+    ProfileController profileController = Get.put(ProfileController());
     return Padding(
-      padding: const EdgeInsets.only(top: 20,right: 10,left: 10,bottom: 10),
+      padding: const EdgeInsets.only(top: 20, right: 10, left: 10, bottom: 10),
       child: SingleChildScrollView(
         child: Form(
           key: profileController.formKey,
           child: Column(
             children: [
-
+              Row(
+                children: [
+                  Icon(
+                    Icons.account_circle,
+                    color: AppColors.bg1LightColor,
+                  ),
+                  SizedBox(height: 10),
+                  CustomTextField(
+                    controller: profileController.oldPasswordController,
+                    labelText: "current_password",
+                  ),
+                  SizedBox(height: 10),
+                ],
+              )
             ],
           ),
         ),
