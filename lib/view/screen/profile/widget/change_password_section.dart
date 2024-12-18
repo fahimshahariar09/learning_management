a4fcd557071d45d4c865845e40c6f759353952ec
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-17 23:33:48 -0800

    first commit

--- lib/view/screen/profile/widget/change_password_section.dart
+++ lib/view/screen/profile/widget/change_password_section.dart
@@ -1,4 +1,3 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:learning_management/controller/ui_controller/profile.dart';
@@ -51,7 +50,7 @@ class ChangePasswordSection extends StatelessWidget {
                 controller: profileController.confPasswordController,
                 labelText: 'conf_password',
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               CustomButton(onTap: ()async{
                 FocusScope.of(context).unfocus();
                 if(!profileController.formKey.currentState!.validate()){
