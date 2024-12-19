4334f78f89e6c1af02ff61d2021fdecce9510d4f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-18 23:23:24 -0800

    first commit

---
+++ lib/view/screen/profile/widget/profile_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ProfileSection extends StatelessWidget {
+  const ProfileSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
