57c1dfb0a8f8f40af7462dc60a196a9340bec49a
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-18 23:51:57 -0800

    first commit

--- lib/view/screen/profile/widget/profile_section.dart
+++ lib/view/screen/profile/widget/profile_section.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning_management/controller/ui_controller/profile.dart';
+import 'package:learning_management/view/common_widget/custom_text_widget.dart';
 import 'package:learning_management/view/screen/profile/widget/change_password_section.dart';
+import 'package:learning_management/view/screen/profile/widget/custom_switch.dart';
 import 'package:learning_management/view/screen/profile/widget/setting_list_tile.dart';
 
 class ProfileSection extends StatelessWidget {
@@ -7,6 +11,7 @@ class ProfileSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ProfileController profileController = Get.put(ProfileController());
     return SizedBox(
       child: SingleChildScrollView(
         child: Padding(
@@ -15,23 +20,37 @@ class ProfileSection extends StatelessWidget {
             children: [
               buildSizedBox(height: 10),
               //change password
-              SettingListTile(onTap: (){
-                showBottomSheet(context: context, builder: (builder){
-                  return const ChangePasswordSection();
-                });
-              }, titel: "change_password"),
+              SettingListTile(
+                  onTap: () {
+                    showBottomSheet(
+                        context: context,
+                        builder: (builder) {
+                          return const ChangePasswordSection();
+                        });
+                  },
+                  titel: "change_password"),
               //change language
               Card(
                 elevation: 4,
                 surfaceTintColor: Colors.transparent,
-                shape: OutlineInputBorder(borderSide: BorderSide.none),
+                shape: const OutlineInputBorder(borderSide: BorderSide.none),
                 child: Row(
                   children: [
-
+                    const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                      child: CustomTextWidget(text: "Language change"),
+                    ),
+                    CustomSwitch(
+                      onTap: () {
+                        profileController.languageBN.value ? Get.updateLocale(const Locale('en','US')) : Get.updateLocale(const Locale('bn','BD'));
+                      },
+                      onText: "onText",
+                      offText: "offText",
+                    )
                   ],
                 ),
               )
-
             ],
           ),
         ),
