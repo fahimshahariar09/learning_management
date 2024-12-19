6c8087850803a72e23009d81955472695062894d
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-18 23:38:05 -0800

    first commit

--- lib/view/screen/profile/widget/custom_switch.dart
+++ lib/view/screen/profile/widget/custom_switch.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CustomSwitch extends StatelessWidget {
-  const CustomSwitch({super.key});
+  const CustomSwitch({super.key, required this.onTap});
+
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell();
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 25,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(color: Colors.grey)
+        ),
+        child: Row(),
+      ),
+    );
   }
 }
