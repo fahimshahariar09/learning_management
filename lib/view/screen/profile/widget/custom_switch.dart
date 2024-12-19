fad5c7534355561dbc46f5b8332487adaa401ed5
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-18 23:40:54 -0800

    first commit

--- lib/view/screen/profile/widget/custom_switch.dart
+++ lib/view/screen/profile/widget/custom_switch.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:learning_management/utlis/app_colors.dart';
 
 class CustomSwitch extends StatelessWidget {
-  const CustomSwitch({super.key, required this.onTap});
+  const CustomSwitch(
+      {super.key,
+      required this.onTap,
+      required this.onText,
+      required this.offText,
+      this.value});
 
   final VoidCallback onTap;
+  final String onText;
+  final String offText;
+  final dynamic value;
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +21,23 @@ class CustomSwitch extends StatelessWidget {
       child: Container(
         height: 25,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          border: Border.all(color: Colors.grey)
+            borderRadius: BorderRadius.circular(5),
+            border: Border.all(color: Colors.grey)),
+        child: Row(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              height: 25,
+              width: 30,
+              decoration: BoxDecoration(
+                  color: value.value == false
+                      ? AppColors.bg1LightColor
+                      : Theme.of(context).cardColor,
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(color: Colors.transparent)),
+            )
+          ],
         ),
-        child: Row(),
       ),
     );
   }
