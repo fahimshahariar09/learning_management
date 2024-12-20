8a88e297f17c68b59b88f8ab64318d7d0d7fd859
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-19 23:33:06 -0800

    first commit

--- lib/view/screen/profile/widget/select_image_dialogue.dart
+++ lib/view/screen/profile/widget/select_image_dialogue.dart
@@ -1,10 +1,30 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:learning_management/view/common_widget/custom_text_widget.dart';
 
 class SelectImageDialogue extends StatelessWidget {
-  const SelectImageDialogue({super.key});
+  const SelectImageDialogue(
+      {super.key, required this.galleryButton, required this.cameraButton});
+
+  final VoidCallback galleryButton;
+  final VoidCallback cameraButton;
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        CustomTextWidget(
+          text: "Selected image",
+          fontSize: 20,
+        ),
+        SizedBox(height: 10),
+        Row(
+          children: [
+            InkWell(
+              onTap: cameraButton,
+            )
+          ],
+        )
+      ],
+    );
   }
 }
