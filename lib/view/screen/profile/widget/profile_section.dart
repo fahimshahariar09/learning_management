aec779e3332dddba40966569d4dfc4f150f41f57
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-19 23:26:35 -0800

    first commit

--- lib/view/screen/profile/widget/profile_section.dart
+++ lib/view/screen/profile/widget/profile_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:learning_management/controller/api_controller/auth/log_out.dart';
 import 'package:learning_management/controller/local_storage/local_storage.dart';
 import 'package:learning_management/controller/ui_controller/profile.dart';
 import 'package:learning_management/view/common_widget/custom_text_widget.dart';
@@ -78,27 +79,37 @@ class ProfileSection extends StatelessWidget {
           text: "do you want to log out?",
           fontSize: 16,
         ),
-    actions: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          InkWell(
-            onTap: (){
-              Get.back();
-            },
-            child: Container(
-              margin: EdgeInsets.symmetric(vertical: 5),
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(10),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              InkWell(
+                onTap: () {
+                  Get.back();
+                },
+                child: Container(
+                  margin: const EdgeInsets.symmetric(vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const CustomTextWidget(
+                    text: "Cancel",
+                    fontColor: Colors.white,
+                  ),
+                ),
               ),
-              child: CustomTextWidget(text: "text"),
-            ),
+              InkWell(
+                onTap: ()async{
+                  var status = await LogOutService.logoutService();
+                  if(status){
+                    await LocalData().deleteData(key: "userInfo");
+                  }
+                },
+              )
+            ],
           )
-        ],
-      )
-    ]
-    );
+        ]);
   }
 
   SizedBox buildSizedBox({double? height, double? width}) => SizedBox(
