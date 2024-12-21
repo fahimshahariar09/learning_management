6c6a193762fa01a0697dc2125b2d9a3663ab4560
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-20 23:20:22 -0800

    first commit

--- lib/view/screen/profile/profile_screen.dart
+++ lib/view/screen/profile/profile_screen.dart
@@ -1,32 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning_management/controller/ui_controller/profile.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15),
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 30,
-              ),
-              SizedBox(height: 10),
-              Row(
-                children: [
-                  Text("data"),
-                  Text("data")
-                ],
-              ),
-              SizedBox(height: 10),
-              Text("data"),
-              SizedBox(height: 10),
-              Text("data"),
-            ],
-          ),
+    Size size = MediaQuery.sizeOf(context);
+    ProfileController profileController = Get.put(ProfileController());
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  width: size.width,
+                  padding: EdgeInsets.only(top: 70, bottom: 20),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
