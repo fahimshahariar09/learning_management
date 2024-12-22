77852ff8346bb0e63e00a4435dc1bf50414f8ca8
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-21 17:10:16 -0800

    first commit

--- lib/controller/ui_controller/mentor/mentor.dart
+++ lib/controller/ui_controller/mentor/mentor.dart
@@ -3,5 +3,5 @@ import 'package:learning_management/model/mentor/top_mentor.dart';
 
 class MentorController extends GetxController{
   RxBool isLoading = true.obs;
-  RxList<TopMentorModel> topmentorlist =
+  List<TopMentorModel> topmentorlist = [];
 }
---
+++ lib/database/mentor/mentor.dart
@@ -0,0 +1,3 @@
+class MentorList {
+  static
+}
