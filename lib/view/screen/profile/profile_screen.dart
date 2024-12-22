4c8d6be64e486544551c53e0530f182bab49892b
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-12-21 16:39:11 -0800

    first commit

--- lib/view/screen/profile/profile_screen.dart
+++ lib/view/screen/profile/profile_screen.dart
@@ -19,117 +19,123 @@ class ProfileScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Stack(
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  width: size.width,
-                  padding: const EdgeInsets.only(top: 70, bottom: 20),
-                  decoration: const BoxDecoration(
-                    color: AppColors.bg1LightColor,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20),
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Get.to(
-                            () => ImageViewWidget(
-                              imageUrl:
-                                  "${profileController.userInfo["profile_image"] ?? ''}",
-                            ),
-                          );
-                        },
-                        child: SizedBox(
-                          height: 100,
-                          child: AspectRatio(
-                            aspectRatio: 1,
-                            child: ClipOval(
-                              child: FadeInImage.assetNetwork(
-                                height: 100,
-                                width: 100,
-                                fit: BoxFit.cover,
-                                placeholder: "",
-                                image:
-                                    "${profileController.userInfo["profile_image"] ?? ''}",
-                                imageErrorBuilder: (context, o, t) {
-                                  return const CircleAvatar(
-                                    backgroundImage: NetworkImage("url"),
-                                  );
-                                },
+            Obx(() => Stack(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      width: size.width,
+                      padding: const EdgeInsets.only(top: 70, bottom: 20),
+                      decoration: const BoxDecoration(
+                        color: AppColors.bg1LightColor,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Get.to(
+                                () => ImageViewWidget(
+                                  imageUrl:
+                                      "${profileController.userInfo["profile_image"] ?? ''}",
+                                ),
+                              );
+                            },
+                            child: SizedBox(
+                              height: 100,
+                              child: AspectRatio(
+                                aspectRatio: 1,
+                                child: ClipOval(
+                                  child: FadeInImage.assetNetwork(
+                                    height: 100,
+                                    width: 100,
+                                    fit: BoxFit.cover,
+                                    placeholder: "",
+                                    image:
+                                        "${profileController.userInfo["profile_image"] ?? ''}",
+                                    imageErrorBuilder: (context, o, t) {
+                                      return const CircleAvatar(
+                                        backgroundImage: NetworkImage("url"),
+                                      );
+                                    },
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      CustomTextWidget(
-                        text:
-                            "${profileController.userInfo["name"] ?? "please update your name"}",
-                        fontWeight: FontWeight.w500,
-                        fontColor: Colors.white,
-                        fontSize: 20,
-                      ),
-                      CustomTextWidget(
-                        text:
-                            "${profileController.userInfo["email"] ?? "empty email"}",
-                        fontColor: Colors.white,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 20,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const CustomTextWidget(
-                            text: "phone",
+                          buildSizedBox(height: 10),
+                          CustomTextWidget(
+                            text:
+                                "${profileController.userInfo["name"] ?? "please update your name"}",
+                            fontWeight: FontWeight.w500,
                             fontColor: Colors.white,
-                            fontWeight: FontWeight.w300,
-                            fontSize: 15,
+                            fontSize: 20,
                           ),
                           CustomTextWidget(
-                              text: "${profileController.userInfo["phone"]}",
-                              fontWeight: FontWeight.w300,
+                            text:
+                                "${profileController.userInfo["email"] ?? "empty email"}",
+                            fontColor: Colors.white,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const CustomTextWidget(
+                                text: "phone",
+                                fontColor: Colors.white,
+                                fontWeight: FontWeight.w300,
+                                fontSize: 15,
+                              ),
+                              CustomTextWidget(
+                                  text:
+                                      "${profileController.userInfo["phone"]}",
+                                  fontWeight: FontWeight.w300,
+                                  fontColor: Colors.white,
+                                  fontSize: 14)
+                            ],
+                          ),
+                          CustomTextWidget(
+                              text:
+                                  "${profileController.userInfo["address"] ?? "empty address"}",
+                              fontSize: 14,
                               fontColor: Colors.white,
-                              fontSize: 14)
+                              fontWeight: FontWeight.w400)
                         ],
                       ),
-                      CustomTextWidget(
-                          text:
-                              "${profileController.userInfo["address"] ?? "empty address"}",
-                          fontSize: 14,
-                          fontColor: Colors.white,
-                          fontWeight: FontWeight.w400)
-                    ],
-                  ),
-                ),
-                Positioned(
-                  child: IconButton(
-                    onPressed: () {
-                      showBottomSheet(
-                          //  isScrollControlled: true,
-                          context: context,
-                          builder: (builder) {
-                            return const ProfileEditSection();
-                          });
-                    },
-                    icon: const Icon(
-                      Icons.edit_note_sharp,
-                      size: 35,
-                      color: Colors.white,
                     ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
+                    Positioned(
+                      child: IconButton(
+                        onPressed: () {
+                          showBottomSheet(
+                              //  isScrollControlled: true,
+                              context: context,
+                              builder: (builder) {
+                                return const ProfileEditSection();
+                              });
+                        },
+                        icon: const Icon(
+                          Icons.edit_note_sharp,
+                          size: 35,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+            buildSizedBox(height: 20),
             const ProfileSection(),
           ],
         ),
       ),
     );
   }
+
+  SizedBox buildSizedBox({double? height, double? width}) => SizedBox(
+        height: height ?? 1,
+        width: width ?? 1,
+      );
 }
