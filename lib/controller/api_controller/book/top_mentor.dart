8ccd692bad5eecbd8d4f77ff9c8dd298ca7cd60c
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-23 23:32:36 -0800

    first commit

--- lib/controller/api_controller/book/top_mentor.dart
+++ lib/controller/api_controller/book/top_mentor.dart
@@ -1,9 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:learning_management/database/mentor/top_mentor_list.dart';
 import 'package:learning_management/model/mentor/top_mentor.dart';
 
 class TopMentorService {
-  static Future<List<TopMentorModel>> tomentorService() async{
-    try {} catch (e) {
+  static Future<List<TopMentorModel>> tomentorService() async {
+    try {
+
+      List<TopMentorModel> data =[];
+
+      for(var i in TopMentorList.topmentorList['topmentor'].toList()){
+        var decodeData = TopMentorModel.formJson(jsonDecode(jsonEncode(i)));
+        data.add(decodeData);
+      }
+      return data;
+    } catch (e) {
       debugPrint("error $e");
     }
     return [];
