a752adc40ba9738fd4e7643978b3226b7d708f66
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-23 23:36:17 -0800

    first commit

--- lib/view/screen/book/book.dart
+++ lib/view/screen/book/book.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learning_management/controller/ui_controller/book/book.dart';
 import 'package:learning_management/view/common_widget/common_search_bar.dart';
+import 'package:learning_management/view/screen/book/widget/book_card.dart';
 
 class Book extends StatelessWidget {
   const Book({super.key});
 
   @override
   Widget build(BuildContext context) {
+    BookController bookController =Get.put(BookController());
     return Scaffold(
       body: Column(
         children: [
@@ -20,16 +24,12 @@ class Book extends StatelessWidget {
           const Text("Top Mentor"),
           Expanded(
             child: GridView.builder(
-              itemCount: 6,
+              itemCount: bookController.topmentorlist.length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3,
               ),
               itemBuilder: (context, index) {
-                return const Column(
-                  children: [
-                    Text("data"),
-                  ],
-                );
+                return BookCard(image: "${}", name: name, titel: titel);
               },
             ),
           ),
