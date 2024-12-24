0fc33f42cb24917932617e538d14db3f2af23030
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-23 23:34:18 -0800

    first commit

--- lib/controller/ui_controller/book/book.dart
+++ lib/controller/ui_controller/book/book.dart
@@ -1,7 +1,15 @@
 import 'package:get/get.dart';
+import 'package:learning_management/controller/api_controller/book/top_mentor.dart';
 import 'package:learning_management/model/mentor/top_mentor.dart';
 
 class BookController extends GetxController{
   RxBool isLoading = true.obs;
   List<TopMentorModel> topmentorlist = [];
+
+  topmentorFun()async{
+    isLoading.value=true;
+    topmentorlist = await TopMentorService.tomentorService();
+    isLoading.value= false;
+  }
+
 }
