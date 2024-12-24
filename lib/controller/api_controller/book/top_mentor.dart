597b652dfd48b39f1475dd319c6fc8244178775b
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-23 23:28:15 -0800

    first commit

--- lib/controller/api_controller/book/top_mentor.dart
+++ lib/controller/api_controller/book/top_mentor.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:learning_management/model/mentor/top_mentor.dart';
 
-class TopMentorService{
-
+class TopMentorService {
+  static Future<List<TopMentorModel>> tomentorService() async{
+    try {} catch (e) {
+      debugPrint("error $e");
+    }
+    return [];
+  }
 }
