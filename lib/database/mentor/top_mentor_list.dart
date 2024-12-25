79a1fc4035a109be23f5680f98864f867986a23c
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-24 23:30:12 -0800

    first commit

--- lib/database/mentor/top_mentor_list.dart
+++ lib/database/mentor/top_mentor_list.dart
@@ -1,12 +1,32 @@
 class TopMentorList {
   static Map<String, dynamic> topmentorList = {
     "topmentor": [
-      {"image": "asset/image/ux_ui.png", "name": "Lisa Rahman", "titel": "UI/UX Design"},
-      {"image": "asset/image/react.png", "name": "Charli Fallon", "titel": "React"},
+      {
+        "image": "asset/image/ux_ui.png",
+        "name": "Lisa Rahman",
+        "titel": "UI/UX Design"
+      },
+      {
+        "image": "asset/image/react.png",
+        "name": "Charli Fallon",
+        "titel": "React"
+      },
       {"image": "asset/image/java.png", "name": "Jennifer", "titel": "Java"},
-      {"image": "asset/image/python.png", "name": "Susan James", "titel": "Python"},
-      {"image": "asset/image/flutter.png", "name": "Jennifer", "Rifat": "Flutter"},
-      {"image": "asset/image/node.png", "name": "Jennifer", "Edward Odell": "Node js"},
+      {
+        "image": "asset/image/python.png",
+        "name": "Susan James",
+        "titel": "Python"
+      },
+      {
+        "image": "asset/image/flutter.png",
+        "name": "Jennifer",
+        "titel": "Flutter"
+      },
+      {
+        "image": "asset/image/node.png",
+        "name": "Edward Odell",
+        "titel": "Node js"
+      },
     ]
   };
 }
