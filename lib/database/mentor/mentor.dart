66a475b605612f8d749267ed066c58662e625db5
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-24 23:50:45 -0800

    first commit

--- lib/database/mentor/mentor.dart
+++ lib/database/mentor/mentor.dart
@@ -1,12 +1,28 @@
 class MentorList {
   static Map<String, dynamic> mentorlist = {
     'mentor': [
-      {"image": "asset/mentor/harris.png", "name": "William Harris", "titel": "C++"},
+      {
+        "image": "asset/mentor/harris.png",
+        "name": "William Harris",
+        "titel": "C++"
+      },
       {"image": "asset/mentor/sylvia.png", "name": "Sylvia", "titel": "PHP"},
       {"image": "asset/mentor/eric.png", "name": "Eric Brann", "titel": "CCNA"},
-      {"image": "asset/mentor/jessie.png", "name": "Jessie William", "titel": "Angular"},
-      {"image": "", "name": "Pamela", "Rifat": "Core Java"},
-      {"image": "", "name": "Maria Smith", "Edward Odell": "C Program"},
+      {
+        "image": "asset/mentor/jessie.png",
+        "name": "Jessie William",
+        "titel": "Angular"
+      },
+      {
+        "image": "asset/mentor/pamela.png",
+        "name": "Pamela",
+        "Rifat": "Core Java"
+      },
+      {
+        "image": "asset/mentor/smith.png",
+        "name": "Maria Smith",
+        "Edward Odell": "C Program"
+      },
     ]
   };
 }
