eee27edb5fed35a06cb91788b4e8966a7e5e1f04
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-24 23:49:12 -0800

    first commit

--- lib/database/mentor/mentor.dart
+++ lib/database/mentor/mentor.dart
@@ -3,8 +3,8 @@ class MentorList {
     'mentor': [
       {"image": "asset/mentor/harris.png", "name": "William Harris", "titel": "C++"},
       {"image": "asset/mentor/sylvia.png", "name": "Sylvia", "titel": "PHP"},
-      {"image": "", "name": "Eric Brann", "titel": "CCNA"},
-      {"image": "", "name": "Jessie William", "titel": "Angular"},
+      {"image": "asset/mentor/eric.png", "name": "Eric Brann", "titel": "CCNA"},
+      {"image": "asset/mentor/jessie.png", "name": "Jessie William", "titel": "Angular"},
       {"image": "", "name": "Pamela", "Rifat": "Core Java"},
       {"image": "", "name": "Maria Smith", "Edward Odell": "C Program"},
     ]
